4f2790a7a2d862746d8225bda8874d7bed50732c
Ben Snell <bensnellstudio@gmail.com>
2021-03-05 12:13:41 -0500

    Draw an image to screen

--- src/main.py
+++ src/main.py
@@ -1,9 +1,22 @@
-from flask import Flask
-app = Flask(__name__)
+import cv2
+import numpy as np
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+def drawImage():
+
+    # Pink background
+    img = np.full((1080,1920,3), (200,200,255),np.uint8)
+
+    # Red circle
+    img = cv2.circle(img, (960, 540), 200, (200, 0, 255), -1)
+
+    # Create the fullscreen window
+    # cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+    cv2.namedWindow("image", cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty("image",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+    cv2.imshow('image', img)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    drawImage()
